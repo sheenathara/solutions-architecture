10cd46f134a511d9d813adedd05b1c3637489560
Sheena Tharakanparampil <sheena.tharakanparampil@harness.io>
2023-12-05 22:05:29 +0000

    Update main.tf

--- internal/terraform/main.tf
+++ internal/terraform/main.tf
@@ -2,7 +2,7 @@ data "harness_platform_organization" "default" {
   identifier = "default"
 }
 
-data "harness_platform_project" "default" {
-  identifier = "Default_Project_1663065042038"
+data "harness_platform_project" "Testim" {
+  identifier = "Testim"
   org_id     = data.harness_platform_organization.default.id
 }
